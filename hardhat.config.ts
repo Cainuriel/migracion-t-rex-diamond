6b6c92de33489fbc131bd1061c3b5c242a8ff968
Fernando Lopez <cainuriel@gmail.com>
2025-06-13 11:29:40 +0200

    taycan network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -37,6 +37,11 @@ const config: HardhatUserConfig = {
       accounts: process.env.ADMIN_WALLET_PRIV_KEY ? [process.env.ADMIN_WALLET_PRIV_KEY] : [],
       gasPrice: 400000000000,
       timeout: 300000, 
+    },
+    taycan: {
+      url: "http://5.250.188.118:8545",
+      accounts: process.env.ADMIN_WALLET_PRIV_KEY ? [process.env.ADMIN_WALLET_PRIV_KEY] : [],
+      timeout: 300000, 
     }
   },
 };
