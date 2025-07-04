c26ede28e86ea4b9965f0102e86c314a44c5d741
Fernando Lopez <cainuriel@gmail.com>
2025-07-04 14:19:57 +0200

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -37,21 +37,6 @@ const config: HardhatUserConfig = {
       accounts: process.env.ADMIN_WALLET_PRIV_KEY ? [process.env.ADMIN_WALLET_PRIV_KEY] : [],
       gasPrice: 400000000000,
       timeout: 300000, 
-    },
-      taycan: {
-      url: "http://5.250.188.118:8545",
-      accounts: process.env.ADMIN_WALLET_PRIV_KEY ? [process.env.ADMIN_WALLET_PRIV_KEY] : [],
-      timeout: 300000, 
-    },
-    alastria: {
-      url: "http://108.142.237.13:8545",
-      accounts: [
-        process.env.ADMIN_WALLET_PRIV_KEY,
-        process.env.INVESTOR1_PRIV_KEY,
-        process.env.INVESTOR2_PRIV_KEY
-      ].filter((key): key is string => !!key),
-      gasPrice: 0, // Alastria es una red sin gas
-      timeout: 300000,
     }
   },
 };
